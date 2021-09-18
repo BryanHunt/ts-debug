f84f60d129363e212e751740d8c6fb9031d65550
Bryan Hunt <bhunt@mac.com>
2021-09-18 10:59:57 -0500

    Initial commit

---
+++ src/greeting.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.greeting = void 0;
+function greeting() {
+    console.log('Hello');
+}
+exports.greeting = greeting;
+//# sourceMappingURL=greeting.js.map
